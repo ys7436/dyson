e9d5b37530be1aac595d160600969cb5ecb1dfa9
ys7436 <38024954+ys7436@users.noreply.github.com>
2018-04-16 09:09:20 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,72 @@
+var _times;
+document.addEventListener("WeixinJSBridgeReady", function (){
+  vedioAutoPlay();
+})
+function vedioAutoPlay(){
+  var audio = document.getElementById("video"),
+    play = function(){
+    audio.play();
+  };
+  audio.play();
+  setTimeout(function(){
+    document.getElementById("video").style.display = "block";
+  }, 500)
+  setTimeout(function () {
+    _times = audio.duration;
+  },600)
+  setTimeout(function () {
+    $('.vedios').animate({
+      opacity: '0'
+    },300,function () {
+      $(this).hide();
+    })
+  },((_times*1000)+600))
+}
+
+$(function () {
+
+  var videos = document.getElementById('video');
+  $('.loading').addClass('active');
+  $(window).on('load', function () {
+    setTimeout(function () {
+      $('.loadMain').fadeOut(600);
+    }, 500);
+    setTimeout(function () {
+      $('#video').css('display','block');
+    },600)
+    time = parseFloat(video.duration);
+  });
+
+
+  // $('.plays').tap(function () {
+  //   if(videos.paused){
+  //     videos.play();
+  //     $(this).addClass('active');
+  //     getInfo();
+  //   }else {
+  //     videos.pause();
+  //     $(this).removeClass('active');
+  //     clearInterval(sets);
+  //   }
+  // })
+  // var time,times,tol,sets;
+  // function getInfo(){
+  //   time = parseFloat(video.duration);
+  //   times = parseFloat(video.currentTime);
+  //   tol = parseFloat(time-times)*1000
+  //   sets = setTimeout(function () {
+  //      $('.vedios').animate({
+  //        opacity:0
+  //      },400,function () {
+  //        $(this).hide();
+  //      })
+  //    },tol);
+  // }
+
+
+
+  $('body').swipeLeft(function(){console.log('向左');})
+  $('body').swipeRight(function(){console.log('向右')})
+  $('body').swipeUp(function(){console.log('向上')})
+  $('body').swipeDown(function(){console.log('向下')})
+})
